6008d261f72d9ae723e94f970f208cee4e7dcab4
Joel Wetzell <git@jwetzell.com>
2026-04-03 18:48:32 -0500

    add playwright layout

---
+++ e2e/modules.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from '@playwright/test'
+
+test('add module', async ({ page }) => {
+    await page.goto('/');
+    await page.locator('app-module-list').getByText('add').click();
+    await page.getByRole('menuitem', { name: 'HTTP Server' }).click();
+})
