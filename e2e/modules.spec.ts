97f1cca090050ec92d0fc28d5bc39683116439e3
Joel Wetzell <git@jwetzell.com>
2026-05-17 16:42:20 -0500

    add todo

--- e2e/modules.spec.ts
+++ e2e/modules.spec.ts
@@ -1,7 +1,8 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('add module', async ({ page }) => {
   await page.goto('/');
   await page.locator('app-module-list').getByText('add').click();
   await page.getByRole('menuitem', { name: 'HTTP Server' }).click();
+  // TODO(jwetzell): test that the module gets added
 });
